02af7097c237730e4a60787d9ed9692a1078844e
Brian <brianla9979@gmail.com>
2025-07-25 01:11:58 +0700

    set up project

---
+++ src/main/java/com/springboot/web_job_search_portal/WebJobSearchPortalApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.web_job_search_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJobSearchPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJobSearchPortalApplication.class, args);
+	}
+
+}
